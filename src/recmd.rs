bdd7043db9b7e05161b5a519bdca7b98586e7a82
kimhoon <kimhoon0316@gmail.com>
2021-06-10 16:29:17 +0900

    dev.ing macos -> .config -> create dir

--- src/recmd.rs
+++ src/recmd.rs
@@ -1,18 +1,35 @@
 use std::collections::HashMap;
+use std::env::consts;
 use std::io::Error;
+use std::fs::{create_dir, OpenOptions};
 
 #[derive(Debug)]
 pub struct ReCmd {
     pub cmd: HashMap<String, String>,
 }
 
+fn store_path() -> &'static str {
+    let os = consts::OS;
+
+    if os == "macos" {
+        return "~/.config/recmd/store.json";
+    }
+
+    "./store.json"
+}
+
 impl ReCmd {
     pub fn new() -> Result<ReCmd, Error> {
-        let f = std::fs::OpenOptions::new()
+        match create_dir("${HOME}/.config/recmd") {
+            Ok(_) => println!("{}", "OK"),
+            Err(e) => panic!("create dir filed {}", e),
+        }
+
+        let f = OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("re.json")?;
+            .open(store_path())?;
 
         match serde_json::from_reader(f) {
             Ok(cmd) => Ok(ReCmd { cmd }),
@@ -28,10 +45,10 @@ impl ReCmd {
     }
 
     pub fn save(self) -> Result<(), Error> {
-        let f = std::fs::OpenOptions::new()
+        let f = OpenOptions::new()
             .write(true)
             .create(true)
-            .open("re.json")?;
+            .open(store_path())?;
 
         serde_json::to_writer_pretty(f, &self.cmd)?;
         Ok(())
@@ -49,11 +66,11 @@ impl ReCmd {
             println!("#\trecmd\tCommand");
             let mut cnt = 1;
             for (k, v) in self.cmd.iter() {
-                println!("{}\t{}\t{}",cnt, k, v);
+                println!("{}\t{}\t{}", cnt, k, v);
                 cnt += 1;
             }
 
-            return
+            return;
         }
 
         println!("recmd command not found");
